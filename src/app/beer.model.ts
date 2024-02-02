65527019317fca3f36018acb84edb97cf5536a7b
Harris Stevenson <harris6144@gmail.com>
2024-02-02 16:29:00 +0000

    Create beer model

---
+++ src/app/beer.model.ts
@@ -0,0 +1,10 @@
+export interface Beer {
+  id: number;
+  name: string;
+  tagline: string;
+  first_brewed: string;
+  description: string;
+  abv: number;
+  ibu: number;
+  food_pairing: string[];
+}
