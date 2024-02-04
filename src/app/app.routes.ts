e78ebbd4fe11ba01e71bf62a75c188963c90a007
Harris Stevenson <harris6144@gmail.com>
2024-02-04 15:05:05 +0000

    Add beer list redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { BeerListComponent } from './beer-list/beer-list.component';
 import { BeerDetailComponent } from './beer-detail/beer-detail.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'beers/1', pathMatch: 'full' },
   { path: 'beers/:page', component: BeerListComponent },
   { path: 'beer/:id', component: BeerDetailComponent }
 ];
